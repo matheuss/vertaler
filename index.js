53452002f7546d0797a1ea18576110347becf857
Matheus <matheus.frndes@gmail.com>
2016-05-09 00:05:53 -0300

    Improved colon notation – source language can now be auto detected

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ var api = require('./api'),
 
 program
     .version(pkg.version)
-    .usage('<options | sourceLang:targetLang> <text>')
+    .usage('<options | <sourceLang>:targetLang> <text>')
     .option('-f, --from <value>', 'Source language', 'en')
     .option('-t, --to <value>', 'Target language', 'nl');
 
-program.on('--help', function(){
+program.on('--help', function () {
     console.log('  Examples:');
     console.log('');
     console.log('    $ vertaler en:nl Hi');
+    console.log('    $ vertaler :en Hoi (automatic source lang detection)');
     console.log('    $ vertaler -f en -t nl Hi');
     console.log('    $ vertaler --from en --to nl Hi');
     console.log('');
@@ -24,12 +25,20 @@ if (!program.args.length) { // called w/o any arguments
     program.help(); // process.exit() is implicit
 }
 
-if (program.args[0].indexOf(':')) { // called with lang:lang
-    if (program.args.length == 1) { // called only w/ lang:lang
+if (program.args[0].indexOf(':') != -1) {
+    if (program.args.length == 1) { // no text to translate
         program.help();
     }
 
     langs = program.args.shift().split(':');
+
+    if (langs[0] == '') { // ':en': translate from auto to english
+        program.from = 'auto';
+        program.to = langs[1];
+    } else if (langs[1] == '') { // 'en:' translate from english to ?
+        program.help();
+    }
+
     program.from = langs[0];
     program.to = langs[1];
 }
