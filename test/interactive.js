f4df2055a8d13f95e8fb5fa3657a8683c6b231fe
Matheus <matheus.frndes@gmail.com>
2016-07-11 00:17:48 -0300

    Add tests

--- test/interactive.js
+++ test/interactive.js
@@ -1,3 +1,17 @@
 import test from 'ava';
+const interactive = require('../interactive')({from: 'en', to: 'pt'});
 
-test.todo('write tests for interactive mode');
+test('translate some text from english to portuguese', async t => {
+    await interactive.exec('en:pt');
+    const translation = await interactive.exec('this is a text in english');
+    t.is(translation, 'este é um texto em Inglês');
+});
+
+test('clear the screen with the \'clear\' command', async t => {
+    await interactive.exec('clear');
+    t.pass();
+});
+
+// no ideas on how to to this
+// already tried: RobotJS
+test.todo('clear the screen by pressing ctrl+l');
